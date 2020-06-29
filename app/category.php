69ab91365834a1d964c1c34f81e9322c1fae70d0
anwarshah <syed.anwar@nextgeni.dev>
2020-06-29 17:41:48 +0500

    relationship updated

---
+++ app/category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class category extends Model
+{
+    //
+
+    public function childs() {
+        return $this->hasMany('App\category','parent_id','id') ;
+    }
+
+
+
+}
