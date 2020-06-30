2b74ced0da13aaeac523ddc372cc5018071fe3a4
anwarshah <syed.anwar@nextgeni.dev>
2020-06-30 16:39:50 +0500

    Added Changes According To FeedBack

--- resources/views/side.blade.php
+++ resources/views/side.blade.php
@@ -3,11 +3,10 @@
    <div class="menu-list">
    <ul id="menu-content" class="menu-content collapse out">
        @foreach(Session::get('category') as $menu)
-
-           <li  data-toggle="collapse"  data-target="<?php if($menu->name == $menu->menu)?><?php echo '#'.str_replace(' ', '', $menu->name)?>" class="collapsed" >         
-             <a href="#"><i class="<?php  echo($menu->name == 'WEB DEVELOPMENT' ? "fa fa-globe fa-lg" : ($menu->name == 'MOBILE APP DEVELOPMENT' ? 'fa fa-phone fa-lg':''));?>"></i>   {{ $menu->name }} <span class="arrow"></span></a>
+           <li  data-toggle="collapse"  data-target="{{ '#'.str_replace(' ', '', $menu->name) }}" class="collapsed" >         
+             <a href="#"><i class='{{$menu->icons}}'></i>   {{ $menu->name }} <span class="arrow"></span></a>
              </li>     
-           <ul class="sub-menu collapse" id="<?php  echo($menu->name == $menu->name ? str_replace(' ', '', $menu->name):'');?>">
+           <ul class="sub-menu collapse" id="<?php  echo str_replace(' ', '', $menu->name);?>">
            @foreach($menu->childs as $child)
             <li  class="<?php  echo (Route::currentRouteName()==strtolower(str_replace(' ', '', $child->name)) ? 'active':'') ?>"> <a href="{{url(strtolower(str_replace(' ', '', $child->name)))}}">{{ $child->name }}</a></li>
             @endforeach    
